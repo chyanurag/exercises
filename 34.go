156f0a805ae243206e402696eb15bd3853124fea
Legion <choudharyanuragm@gmail.com>
2024-08-01 19:48:16 +0530

    removed extra files

--- 34.go
+++ 34.go
@@ -1,8 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func findSlice(s []string, target string) int {
+	for i, v := range s {
+		if v == target {
+			return  i
+		}
+	}
+	return -1
+}
+
+func printEmployees(emps []string) {
+	fmt.Printf("There are %d employees\n", len(emps))
+	for _, v := range emps {
+		fmt.Println(v)
+	}
+}
+
 func main() {
 
-    
-
-
+	employees := []string{"John Smith", "Jackie Jackson", "Chris Jones", "Amanda Cullen", "Jeremy Goodwin"}
+	for {
+		printEmployees(employees)
+		fmt.Print("Enter an employee name to remove : ")
+		sc := bufio.NewScanner(os.Stdin)
+		sc.Scan()
+		emp := sc.Text()
+		pos := findSlice(employees, emp)
+		if pos == -1 {
+			fmt.Println("Employee does not exists")
+		} else {
+			employees = append(employees[:pos], employees[pos+1:]...)
+		}
+	}
 }
--- 35.go
+++ 35.go
@@ -1,8 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"math/rand"
+)
+
 func main() {
 
-    
 
+	names := []string{}
+
+	for {
+		fmt.Print("Enter a name : ")
+		sc := bufio.NewScanner(os.Stdin)
+		sc.Scan()
+		name := sc.Text()
+		if name == "" {
+			break
+		}
+		names = append(names, name)
+	}
+	n := rand.Intn(len(names))
+	fmt.Printf("The winner is %s\n", names[n])
 
 }
